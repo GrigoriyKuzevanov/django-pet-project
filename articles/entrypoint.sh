fbf5824f096f120cad6085a800a9e095d5df070f
Григорий Кузеванов <grkuzevanov@gmail.com>
2023-12-29 14:43:16 +0500

    add docker-compose

---
+++ articles/entrypoint.sh
@@ -0,0 +1,11 @@
+PYTHONUNBUFFERED=True
+
+python3 manage.py makemigrations --no-input
+python3 manage.py makemigrations users --no-input
+python3 manage.py makemigrations blog --no-input
+
+python3 manage.py migrate --no-input
+
+python3 manage.py collectstatic --no-input
+
+gunicorn articles.wsgi:application -b 0.0.0.0:8000 --reload
