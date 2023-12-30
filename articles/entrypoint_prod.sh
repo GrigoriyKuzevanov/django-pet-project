ae3fb5d305b0b59ac49afec05b3dd0ffe79dcae1
Григорий Кузеванов <grkuzevanov@gmail.com>
2023-12-30 12:12:42 +0500

    add docker-compose for prod

--- articles/entrypoint_prod.sh
+++ articles/entrypoint_prod.sh
@@ -4,4 +4,6 @@ python3 manage.py makemigrations blog --no-input
 
 python3 manage.py migrate --no-input
 
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py collectstatic --no-input
+
+gunicorn articles.wsgi:application -b 0.0.0.0:8000 --reload
