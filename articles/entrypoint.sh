8edfbd50ca82a3451b99aa105c9d511084efca89
Григорий Кузеванов <grkuzevanov@gmail.com>
2023-12-30 11:45:08 +0500

    add docker-compose for local

--- articles/entrypoint.sh
+++ articles/entrypoint.sh
@@ -1,11 +1,10 @@
-PYTHONUNBUFFERED=True
-
 python3 manage.py makemigrations --no-input
 python3 manage.py makemigrations users --no-input
 python3 manage.py makemigrations blog --no-input
 
 python3 manage.py migrate --no-input
 
-python3 manage.py collectstatic --no-input
+# python3 manage.py collectstatic --no-input
 
-gunicorn articles.wsgi:application -b 0.0.0.0:8000 --reload
+python3 manage.py runserver 0.0.0.0:8000
+# gunicorn articles.wsgi:application -b 0.0.0.0:8000 --reload
